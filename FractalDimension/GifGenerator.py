3c299a0416213a8e6b9c4efdc1e5bc57c6e56237
ZefCo <zefram.cochrane2024@gmail.com>
2023-07-21 12:48:10 -0500

    Updating to work on a single gene time embedding

--- FractalDimension/GifGenerator.py
+++ FractalDimension/GifGenerator.py
@@ -6,9 +6,41 @@ import os
 import shutil
 
 
-
 def main():
     '''
+    '''
+    various_k_plus()
+
+
+
+def various_k_plus():
+    '''
+    '''
+
+    file = str(cwd.parent / "ML" / "TrainingData_SameSize.pkl")
+    
+    k_m = 9
+    max_rows = 5000
+    gap = 0 
+    k_p_min, k_p_max, k_p_step = 0, 9, 1
+
+    image_dir = cwd / "TE_Images"
+    exon_dir = image_dir / "Exon"
+    intron_dir = image_dir / "Intron"
+    both_dir = image_dir / "Both"
+    
+    for k_p in range(k_p_min, k_p_max + k_p_step, k_p_step):
+        print(f"### K Plus = {k_p} ###")
+        TE.time_embedding_v2(file, k_p = k_p, k_m = k_m, max_rows = max_rows, gap = gap)
+
+    generator(exon_dir, str(cwd / f"Exons_gap_{k_p_min}_{k_p_max}.gif"))
+    generator(intron_dir, str(cwd / f"Introns_gap_{k_p_min}_{k_p_max}.gif"))
+    generator(both_dir, str(cwd / f"Both_gap_{k_p_min}_{k_p_max}.gif"))
+
+
+def various_gaps():
+    '''
+    Adjusts values for different gaps, looking at a 9 v 6 history
     '''
 
     k_p = 6
