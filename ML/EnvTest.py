0430da2cad58c729207412bb4897934853f28157
ZefCo <zefram.cochrane2024@gmail.com>
2024-01-08 11:15:32 -0600

    Pushing to pull

--- ML/EnvTest.py
+++ ML/EnvTest.py
@@ -1,16 +1,24 @@
+import os
+# os.environ["GET_PYTHON_REFRESH"] = "quiet"
+# import git
 import pandas
 import pathlib
+import shutil
 import re
 # from tensorflow.keras.models import Sequential, load_module
 # from tensorflow.keras.layers import Conv1D
 # from sklearn.model_selection import train_test_split
 # from sklearn.metrics import accuracy_score
-import tensorflow as tf
+# import tensorflow as tf
+# print(tf.__version__)
+# exit()
 import random
 import numpy as np
 import pandas as pd
 # import plotly.graph_objects as go
+import datetime
 from matplotlib import pyplot as plt
-import git
+from contextlib import redirect_stdout
+from PIL import Image
 
-print(tf.__version__)
+print("Env Test complete: passed")
