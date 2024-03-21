b0e2801ab45082be900b32d69957045c3423c874
Ethan Speakman <espeakma@carya.rcdc.uh.edu>
2024-03-21 11:54:54 -0500

    Trying to track down env issues

--- ML/EnvTest.py
+++ ML/EnvTest.py
@@ -1,26 +1,15 @@
 import os
-# os.environ["GET_PYTHON_REFRESH"] = "quiet"
-# import git
 import shutil
 import pandas
 import pathlib
 import shutil
 import re
 import sys
-# from tensorflow.keras.models import Sequential, load_module
-# from tensorflow.keras.layers import Conv1D
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score
 # import tensorflow as tf
 # print(tf.__version__)
-# exit()
 import random
-# import numpy as np
-# import pandas as pd
-# import plotly.graph_objects as go
 import datetime
 from matplotlib import pyplot as plt
-from contextlib import redirect_stdout
 from PIL import Image
 
 print("Env Test complete: passed")
